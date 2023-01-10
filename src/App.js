0b4ed77fddbcece511cf4072e783f80791365aa8
toqha0307 <rinum01@naver.com>
2023-01-10 23:27:30 +0900

    밥주기 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 import cat from './images/cat-FirstMeet.png'
 import catFood from './images/catFood.png'
 import catToy from './images/catToy.png'
 import Box from './images/textBoxLong.png'
 import feedFood from './pages/FeedFood.js'
-import updateHeart from './pages/MsgText.js'
+import MsgText from './pages/MsgText.js'
 import './App.css';
 
 function App() {
+  const [heart, setHeart] = useState(parseInt(localStorage.getItem("heart")));
+  const feedFood = () => {
+                          setHeart(heart+1); 
+                          localStorage.setItem("heart",heart);
+                        }
+
   return (
     <div className="App">
       <nav>
@@ -22,7 +28,9 @@ function App() {
                     <img src={Box} alt="Box"  width="1200"/>
                 </div>
                 <div class="msgBoxText">
-                    <MsgText/>
+                  <div class="msgBoxText">
+                      <p>냐옹이가 {heart}개 만큼 맛있게 먹었습니다.</p>
+                  </div>
                 </div>
             </div>
         </div>
--- src/pages/MsgText.js
+++ src/pages/MsgText.js
@@ -5,3 +5,5 @@ function MsgText(props){
         </div>
     )
 }
+
+export default MsgText ;
