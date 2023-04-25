3b159612a8aca65aec75e57d5d03e499d29a6da5
toqha0307 <sbkwon0307@gmail.com>
2023-04-26 00:07:53 +0900

    이미지변경

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React,{useState,useEffect,useRef} from "react";
 import cat from './images/cat-FirstMeet.png'
+import cat2 from './images/cat-FirstMeet2.png'
 import catFood from './images/catFood.png'
+import catBook2 from './images/catBook2.png'
 import catToy from './images/catToy.png'
 import Box from './images/textBoxLong.png'
 import feedFood from './pages/FeedFood.js'
 import MsgText from './pages/MsgText.js'
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const sendRequest = async() => {
+    const response = await axios.get('http://localhost:8090');
+    console.log(response);
+    console.log(response.data);
+  }
+
   let startHeart = (localStorage.getItem("heart"))==null? 0 : parseInt(localStorage.getItem("heart"));
   const [heart, setHeart] = useState(startHeart);
+  const [catImg, setCatImg] = useState(0);
+  const [intervlId, setIntervalId] = useState(null);
   let timeoutId = useRef(null);
+  const images = [cat, cat2];
   const changeText = () => {
                         if(heart<10) return (`냐옹이가 아직 당신을 낯설어합니다.`)
                         else if(heart>=10&&heart<30) return ("냐옹이가 당신을 조금은 기억합니다.")
@@ -21,20 +33,29 @@ function App() {
                           setHeart(updateHeart); 
                           localStorage.setItem("heart",updateHeart)
                           setMsgText(`냐옹이가 ${updateHeart}개 만큼 맛있게 먹었습니다!`)
+                          clearTimeout(timeoutId.current);
                           timeoutId.current = setTimeout(()=>{
                             setMsgText(changeText())
-                          },3000);
-                        }
+                          },1300);
+                      }
+  const playWith = () => {
+
+                      }
   const [msgText, setMsgText] = useState(changeText());
   useEffect(()=>{
-      return () => clearTimeout(timeoutId.current);
-  },[]);                      
+    sendRequest();
+    const id = setInterval(()=>{
+      setCatImg(catImg=>(catImg+1)%images.length);
+    },1000);
+    setIntervalId(id);
+    return () => clearInterval(id);
+  },[catImg]);                    
   return (
     <div className="App">
       <nav>
-        <div className="App"></div>
         <div class="mainDiv">  
-            <img src={cat} alt="cat" width="500"/>  
+            <img src={images[catImg]} alt="cat" width="500"/> 
+            <img src={catBook2} alt="catBook" width="300"/>
             <img src={catFood} alt="catFood" width="300" 
                 onClick= {feedFood}/>
             <img src={catToy} alt="catToy" width="300"/>
