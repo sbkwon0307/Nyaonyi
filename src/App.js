3531145d227576737dcf6bf5ad0217bf68af15a3
toqha0307 <sbkwon0307@gmail.com>
2023-04-26 22:37:01 +0900

    컴포넌트분리

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import catFood from './images/catFood.png'
 import catBook2 from './images/catBook2.png'
 import catToy from './images/catToy.png'
 import Box from './images/textBoxLong.png'
-import feedFood from './pages/FeedFood.js'
-import MsgText from './pages/MsgText.js'
+import feedFood from './components/FeedFood.js'
+import MsgText from './components/MsgText.js'
 import './App.css';
 import axios from 'axios';
 
@@ -17,30 +17,18 @@ function App() {
     console.log(response.data);
   }
 
-  let startHeart = (localStorage.getItem("heart"))==null? 0 : parseInt(localStorage.getItem("heart"));
-  const [heart, setHeart] = useState(startHeart);
   const [catImg, setCatImg] = useState(0);
   const [intervlId, setIntervalId] = useState(null);
   let timeoutId = useRef(null);
   const images = [cat, cat2];
-  const changeText = () => {
-                        if(heart<10) return (`냐옹이가 아직 당신을 낯설어합니다.`)
-                        else if(heart>=10&&heart<30) return ("냐옹이가 당신을 조금은 기억합니다.")
-                        else if(heart>30) return ("냐옹이가 조금 경계를 풀었습니다.")
-                        }
-  const feedFood = () => {
-                          let updateHeart = heart + 1
-                          setHeart(updateHeart); 
-                          localStorage.setItem("heart",updateHeart)
-                          setMsgText(`냐옹이가 ${updateHeart}개 만큼 맛있게 먹었습니다!`)
-                          clearTimeout(timeoutId.current);
-                          timeoutId.current = setTimeout(()=>{
-                            setMsgText(changeText())
-                          },1300);
-                      }
   const playWith = () => {
 
-                      }
+             
+    const changeText = () => {
+      if(heart<10) return (`냐옹이가 아직 당신을 낯설어합니다.`)
+      else if(heart>=10&&heart<30) return ("냐옹이가 당신을 조금은 기억합니다.")
+      else if(heart>30) return ("냐옹이가 조금 경계를 풀었습니다.")
+  }         }
   const [msgText, setMsgText] = useState(changeText());
   useEffect(()=>{
     sendRequest();
@@ -56,8 +44,6 @@ function App() {
         <div class="mainDiv">  
             <img src={images[catImg]} alt="cat" width="500"/> 
             <img src={catBook2} alt="catBook" width="300"/>
-            <img src={catFood} alt="catFood" width="300" 
-                onClick= {feedFood}/>
             <img src={catToy} alt="catToy" width="300"/>
             <div class="msgBoxWrap">
                 <div class="msgBoxImg">
---
+++ src/components/FeedFood.js
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+
+
+const FeedButton = () => {
+    let startHeart = (localStorage.getItem("heart"))==null? 0 : parseInt(localStorage.getItem("heart"));
+    const [heart, setHeart] = useState(startHeart);
+
+
+    const handleFeedButtonClick = () => {
+        let updateHeart = heart + 1
+        setHeart(updateHeart); 
+        localStorage.setItem("heart",updateHeart)
+        setMsgText(`냐옹이가 ${updateHeart}개 만큼 맛있게 먹었습니다!`)
+        clearTimeout(timeoutId.current);
+        timeoutId.current = setTimeout(()=>{
+          setMsgText(changeText())
+        },1300);
+    }
+
+
+    return (
+        <div>
+            <img src="../images/catFood.png" alt="catFood" width="300" onClick= {handleFeedButtonClick}/>
+        </div>
+    );
+};
+
+export default FeedButton;
+
